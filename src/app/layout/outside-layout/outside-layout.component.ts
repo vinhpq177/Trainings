1c4ccde6ff2bce970f666a40d832c41856e4793b
vinh.phamquang <vinh.phamquang@ascendcorp.com>
2021-03-25 22:27:18 +0700

    [Louis] Commit Includes : 1- Separate outside/inside layouts 2- Declared css libs into angular.json

---
+++ src/app/layout/outside-layout/outside-layout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-outside-layout',
+  templateUrl: './outside-layout.component.html',
+  styleUrls: ['./outside-layout.component.css']
+})
+export class OutsideLayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
